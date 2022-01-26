9a0b1b49a65a5c952a15dffe1e5d59983a123d53
Denis <varenikovda@gmail.com>
2022-01-26 22:10:01 +0300

    Update project

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var helloWorlLableNew: UILabel!
+    
+    
+    @IBOutlet var buttonTestNew: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorlLableNew.isHidden = true
+        helloWorlLableNew.textColor = .systemRed
+        buttonTestNew.layer.cornerRadius = 10
     }
 
-
+    @IBAction func buttonActionTest() {
+        helloWorlLableNew.isHidden.toggle()
+        
+        if helloWorlLableNew.isHidden {
+            buttonTestNew.setTitle("Show text", for: .normal)
+        } else {
+            buttonTestNew.setTitle("Hide text", for: .normal)
+        }
+    }
+    
+    
 }
 
